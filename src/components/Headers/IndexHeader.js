b1406e4d9e3dfb3385745148fcafad2da82bd948
Albert Luganga <ahosea15@gmail.com>
2020-01-15 22:21:56 +0300

    change title

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -38,7 +38,7 @@ function IndexHeader() {
         <div className="content-center">
           <Container>
             <div className="title-brand">
-              <h1 className="presentation-title">Paper Kit React</h1>
+              <h1 className="presentation-title">Finder</h1>
               <div className="fog-low">
                 <img alt="..." src={require("assets/img/fog-low.png")} />
               </div>
@@ -47,7 +47,7 @@ function IndexHeader() {
               </div>
             </div>
             <h2 className="presentation-subtitle text-center">
-              Make your mark with a Free Bootstrap 4 (Reactstrap) UI Kit!
+              Do checks effectively!
             </h2>
           </Container>
         </div>
